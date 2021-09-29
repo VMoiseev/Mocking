72e77f0f1da854f03dde3ac52a632cc560419787
VMoiseev <e175@inbox.ru>
2021-09-29 07:25:36 +0500

    Доработка для прохождения тестов

--- src/js/__tests__/app.test.js
+++ src/js/__tests__/app.test.js
@@ -7,14 +7,15 @@ beforeEach(() => {
   jest.resetAllMocks();
 });
 
-test('Проверка функции если статус нормальный и если статус ошибочен', () => {
+test('Проверка функции если статус нормальный', () => {
   fetchData.mockReturnValue({
     status: 'ok',
     level: 5,
   });
   expect(getLevel(1)).toBe('Ваш текущий уровень: 5');
-  expect(fetchData).toBeCalledWith('https://server/user/1');
+});
 
+test('Проверка функции если статус ошибочен', () => {
   fetchData.mockReturnValue({
     status: 'error',
     level: 5,
